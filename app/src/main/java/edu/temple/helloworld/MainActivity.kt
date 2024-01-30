a40fde518b860abe6cdccc8d0c233f8491929f62
MinJoon Joo <tuk34896@temple.edu>
2024-01-30 12:14:37 -0500

    created responses

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -10,6 +10,8 @@ class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
+    lateinit var click : Button
+    lateinit var edit: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +19,14 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
-
+        click = findViewById(R.id.clickMeButton)
+        edit = findViewById(R.id.nameEditText)
         
         // Respond to button click event per specifications
-
+        click.setOnClickListener{
+            val name =edit.text.toString()
+            displayTextView.text= "Hello, $name"
+        }
 
     }
 }
